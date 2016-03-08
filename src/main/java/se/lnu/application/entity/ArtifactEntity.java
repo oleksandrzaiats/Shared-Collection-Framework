dc661884c8df65a99992999b41aad30eeb4fa26e
zayats <sanyazayats@gmail.com>
2016-03-08 17:56:47 +0100

    Finish artifact entity

--- src/main/java/se/lnu/application/entity/ArtifactEntity.java
+++ src/main/java/se/lnu/application/entity/ArtifactEntity.java
@@ -10,11 +10,55 @@ public class ArtifactEntity implements CommonEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "fileName")
+    private String fileName;
+
+    @Column(name = "contentType")
+    private String contentType;
+
+    @Column(name = "file")
+    private byte[] file;
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getFile() {
+        return file;
+    }
+
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
     }
 }
