d4e2f75a1f8f8bd3312b3bd9531e5cd7851e46e7
zayats <sanyazayats@gmail.com>
2016-03-08 17:57:02 +0100

    Finish artifact DTO

--- src/main/java/se/lnu/application/dto/ArtifactDTO.java
+++ src/main/java/se/lnu/application/dto/ArtifactDTO.java
@@ -1,5 +1,52 @@
 package se.lnu.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class ArtifactDTO implements CommonDTO {
-    long id;
+    private Long id;
+    private String name;
+    private String fileName;
+    private String contentType;
+    @JsonIgnore
+    private byte[] fileBytes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getFileBytes() {
+        return fileBytes;
+    }
+
+    public void setFileBytes(byte[] fileBytes) {
+        this.fileBytes = fileBytes;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
 }
