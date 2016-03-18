0847ed3f800d8d294ca11f46ec211416ba925773
olefiir <nastia.olefir@gmail.com>
2016-03-19 00:14:10 +0100

    Added registration

--- src/main/java/se/lnu/application/controller/AuthenticationController.java
+++ src/main/java/se/lnu/application/controller/AuthenticationController.java
@@ -6,9 +6,12 @@
 package se.lnu.application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import se.lnu.application.dto.UserDTO;
 import se.lnu.application.processor.UserProcessor;
+import se.lnu.application.security.UserRole;
 import se.lnu.application.security.token.TokenAuthenticationService;
 
 import javax.servlet.http.HttpServletResponse;
@@ -28,13 +31,37 @@ public class AuthenticationController {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public @ResponseBody
-    String createTokenForUser(@RequestParam("login") String login, @RequestParam("password") String password, HttpServletResponse response) {
+    ResponseEntity<?>  createTokenForUser(@RequestParam("login") String login, @RequestParam("password") String password, HttpServletResponse response) {
         UserDTO authUser = userProcessor.findUserByLogin(login);
 
         if (authUser != null & authUser.getPassword().equals(password)) {
-            return tokenAuthenticationService.addAuthentication(response, authUser);
+            return new ResponseEntity<>(tokenAuthenticationService.addAuthentication(response, authUser), HttpStatus.OK);
         } else {
-            return "User isn't found";
+            return new ResponseEntity<>("User isn't found", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public @ResponseBody
+    ResponseEntity<?> createTokenForUser(@RequestParam("login") String login, @RequestParam("name") String name, @RequestParam("password") String password, HttpServletResponse response) {
+        UserDTO authUser = userProcessor.findUserByLogin(login);
+
+        if (authUser == null) {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setLogin(login);
+            userDTO.setName(name);
+            userDTO.setPassword(password);
+            userDTO.setRole(UserRole.USER.toString());
+
+            userDTO = userProcessor.create(userDTO);
+
+            if (userDTO != null) {
+                return new ResponseEntity<>("User is created", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("User isn't created", HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            return new ResponseEntity<>("User exits", HttpStatus.BAD_REQUEST);
         }
     }
 }
