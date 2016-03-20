bc59c33b7b2abd3d53b3fcb66e346c7ffaa0f703
olefiir <nastia.olefir@gmail.com>
2016-03-20 17:48:00 +0100

    Added global CORS configuration

--- src/main/java/se/lnu/configuration/SpringWebConfig.java
+++ src/main/java/se/lnu/configuration/SpringWebConfig.java
@@ -2,6 +2,7 @@ package se.lnu.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
  
@@ -9,4 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @ComponentScan({"se.lnu"})
 public class SpringWebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
