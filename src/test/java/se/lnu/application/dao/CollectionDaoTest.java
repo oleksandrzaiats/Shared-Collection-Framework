4b59728e9602bb9f62af79562bab2a67392bb3fe
olefiir <nastia.olefir@gmail.com>
2016-03-23 21:42:53 +0100

    Added tests for dao and controllers

---
+++ src/test/java/se/lnu/application/dao/CollectionDaoTest.java
@@ -0,0 +1,133 @@
+package se.lnu.application.dao;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import se.lnu.application.entity.ArtifactEntity;
+import se.lnu.application.entity.CollectionEntity;
+import se.lnu.application.entity.UserEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionDaoTest extends BaseDaoTest<CollectionEntity> {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Autowired
+    private ArtifactDAO artifactDAO;
+
+    private UserEntity userEntity;
+    private ArtifactEntity artifactEntity;
+
+    private CollectionEntity entity;
+
+    @Before
+    public void setUp() {
+        userEntity = InitialValue.getUserEntity();
+        userDAO.create(userEntity);
+
+        artifactEntity = InitialValue.getArtifactEntity(userEntity);
+        artifactDAO.create(artifactEntity);
+
+        List<ArtifactEntity> artifactEntities = new ArrayList<>();
+        artifactEntities.add(artifactEntity);
+
+        Assert.assertNotNull(userEntity.getId());
+        Assert.assertNotNull(artifactEntity.getId());
+
+        entity = InitialValue.getCollectionEntity(userEntity, artifactEntities, null);
+    }
+
+    @Test
+    public void testAdd() {
+        this.baseCreate(entity);
+
+        Assert.assertNotNull(this.baseGet(entity.getId()));
+    }
+
+    @Test
+    public void testGetList() {
+        this.baseCreate(entity);
+
+        List<CollectionEntity> list = this.baseGetList(new ArrayList<>());
+
+        Assert.assertFalse(list.isEmpty());
+        Assert.assertNotNull(list.get(0));
+        Assert.assertTrue(list.get(0).getName().equals("test"));
+    }
+
+    @Test
+    public void testMergeList() {
+        this.baseCreate(entity);
+
+        List<CollectionEntity> listOld = this.baseGetList(new ArrayList<>());
+        Assert.assertFalse(listOld.isEmpty());
+
+        entity.setName("new_test");
+        List<CollectionEntity> listAdd = new ArrayList<>();
+        listAdd.add(entity);
+        this.baseMergeList(listAdd);
+
+        List<CollectionEntity> listNew = this.baseGetList(new ArrayList<>());
+
+        Assert.assertFalse(listNew.isEmpty());
+        Assert.assertTrue(listNew.size() == listOld.size());
+        Assert.assertEquals(this.baseGet(entity.getId()).getName(), "new_test");
+    }
+
+    @Test
+    public void testSaveList() {
+        this.baseCreate(entity);
+
+        List<CollectionEntity> listOld = this.baseGetList(new ArrayList<>());
+        Assert.assertFalse(listOld.isEmpty());
+
+        List<ArtifactEntity> artifactEntities = new ArrayList<>();
+        artifactEntities.add(artifactEntity);
+
+        CollectionEntity newEntity = InitialValue.getCollectionEntity(userEntity, artifactEntities, null);
+        List<CollectionEntity> listAdd = new ArrayList<>();
+        listAdd.add(newEntity);
+        this.baseSaveList(listAdd);
+
+        List<CollectionEntity> listNew = this.baseGetList(new ArrayList<>());
+
+        Assert.assertFalse(listNew.isEmpty());
+        Assert.assertTrue(listNew.size() > listOld.size());
+        Assert.assertTrue(listNew.size() - listOld.size() == 1);
+    }
+
+    @Test
+    public void testCreate() {
+        this.baseCreate(entity);
+        Assert.assertNotNull(entity.getId());
+    }
+
+    @Test
+    public void testUpdate() {
+        this.baseCreate(entity);
+
+        entity.setName("new_test_value");
+        this.baseUpdate(entity);
+
+        Assert.assertNotNull(entity);
+        Assert.assertEquals(this.baseGet(entity.getId()).getName(), "new_test_value");
+    }
+
+    @Test
+    public void testDelete() {
+        this.baseCreate(entity);
+        this.baseDelete(entity);
+        Assert.assertNull(this.baseGet(entity.getId()));
+    }
+
+    @Test
+    public void testGet() {
+        this.baseCreate(entity);
+
+        this.baseGet(entity.getId());
+    }
+}
---
+++ src/test/java/se/lnu/application/dao/InitialValue.java
@@ -0,0 +1,46 @@
+package se.lnu.application.dao;
+
+import se.lnu.application.entity.UserEntity;
+import se.lnu.application.entity.ArtifactEntity;
+import se.lnu.application.entity.CollectionEntity;
+import se.lnu.application.security.UserRole;
+
+import java.util.List;
+
+/**
+ * Created by olefir on 2016-03-20.
+ */
+public class InitialValue {
+
+    public static ArtifactEntity getArtifactEntity(UserEntity userEntity) {
+        ArtifactEntity artifactEntity = new ArtifactEntity();
+        artifactEntity.setName("test");
+        artifactEntity.setContentType("test");
+        artifactEntity.setFile(new byte[]{'t', 'e', 's', 't'});
+        artifactEntity.setFileName("test");
+        artifactEntity.setUser(userEntity);
+
+        return artifactEntity;
+    }
+
+    public static UserEntity getUserEntity() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setName("test");
+        userEntity.setLogin("login");
+        userEntity.setRole(UserRole.ROLE_USER.toString());
+        userEntity.setPassword("test");
+
+        return userEntity;
+    }
+
+    public static CollectionEntity getCollectionEntity(UserEntity userEntity, List<ArtifactEntity> artifactEntityList, List<CollectionEntity> collectionEntityList) {
+        CollectionEntity collectionEntity = new CollectionEntity();
+        collectionEntity.setName("test");
+        collectionEntity.setUser(userEntity);
+        collectionEntity.setArtifactList(artifactEntityList);
+        collectionEntity.setCollectionList(collectionEntityList);
+        collectionEntity.setKey("test");
+
+        return collectionEntity;
+    }
+}
