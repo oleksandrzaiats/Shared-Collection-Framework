df19271f996c618d56c67f8e68c30b4ef00daebc
Patriotozz <volonteer@gmail.com>
2016-03-24 00:25:18 +0100

    Constraints annotations added
    Property level constraints used, but not a filed level.

--- src/main/java/se/lnu/application/model/dto/UserDTO.java
+++ src/main/java/se/lnu/application/model/dto/UserDTO.java
@@ -3,22 +3,26 @@ package se.lnu.application.model.dto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class UserDTO implements CommonDTO {
 
     private Long id;
-    @NotNull
-    @Size(min = 3, max = 255)
+    
     private String login;
     private String name;
-    @NotNull
-    @Size(min = 3, max = 255)
+    
     private String password;
     @JsonIgnore
     private String role;
-
+    
+    @NotNull
+    @Min(1)
+    @Max(value = Long.MAX_VALUE)
     public Long getId() {
         return id;
     }
@@ -26,16 +30,25 @@ public class UserDTO implements CommonDTO {
     public void setId(Long id) {
         this.id = id;
     }
-
+    
+    @NotNull
+    @Pattern(regexp="/^[a-z0-9_-]{3,20}$/")
     public String getLogin() {
         return login;
     }
 
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z]{3,40}$")
     public String getName() {
         return name;
     }
 
     @JsonIgnore
+    @Pattern(regexp = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{6,20})$/", message="Password "
+    		+ "must be from 6 to 20 characters long, "
+    		+ "must contain one Upper case, "
+    		+ "one Lower case, "
+    		+ "one special symbol: !,@,#,$,%,^,&,*,")
     public String getPassword() {
         return password;
     }
