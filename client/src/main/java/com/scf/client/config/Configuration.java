929b8bc384b44f469b822cc162b5408978f28d56
zaiats103 <sanyazayats@gmail.com>
2016-03-27 01:27:35 +0100

    change configuration to production one

--- client/src/main/java/com/scf/client/config/Configuration.java
+++ client/src/main/java/com/scf/client/config/Configuration.java
@@ -2,8 +2,8 @@ package com.scf.client.config;
 
 public interface Configuration {
 
-//    final String SERVER_URL="https://188.166.160.8/";
-    final String SERVER_URL="http://localhost:8080/";
+    final String SERVER_URL="https://188.166.160.8/";
+//    final String SERVER_URL="http://localhost:8080/";
 
     String getServerUrl();
 }
