37333a0ef03a43e0547e2e1f501d2f1153aebc58
olefiir <nastia.olefir@gmail.com>
2016-04-07 09:43:30 +0200

    Added registration

--- android/src/main/java/com/scf/android/activity/UpdateActivity.java
+++ android/src/main/java/com/scf/android/activity/UpdateActivity.java
@@ -2,7 +2,10 @@ package com.scf.android.activity;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -14,8 +17,12 @@ import com.scf.android.SCFApplication;
 import com.scf.client.SCFClient;
 import com.scf.shared.dto.ArtifactDTO;
 
+import java.io.File;
+import java.net.URISyntaxException;
+
 public class UpdateActivity extends AbstractActivity  {
     private EditText mNameView;
+    private EditText mPathView;
     private SCFClient scfClient;
 
     private View mProgressView;
@@ -25,12 +32,15 @@ public class UpdateActivity extends AbstractActivity  {
 
     private static final int FILE_SELECT_CODE = 0;
 
+    public static final String ARTIFACT_ID_EXTRA = "artifact_id";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
 
         mNameView = (EditText) findViewById(R.id.update_artifact_name);
+        mPathView = (EditText) findViewById(R.id.update_artifact_path);
 
         Button mCreateArtifact = (Button) findViewById(R.id.update_button);
         mCreateArtifact.setOnClickListener(new View.OnClickListener() {
@@ -40,21 +50,54 @@ public class UpdateActivity extends AbstractActivity  {
             }
         });
 
+        Button mChooseArtifact = (Button) findViewById(R.id.choose_updated_button);
+        mChooseArtifact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseArtifact();
+            }
+        });
+
         SCFApplication application = (SCFApplication) UpdateActivity.this.getApplication();
         scfClient = application.getSCFClient();
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            Long value = extras.getLong("artifact_id");
-            artifact = scfClient.getArtifact(value);
-            mNameView.setText(artifact.getName());
+            Long value = extras.getLong(ARTIFACT_ID_EXTRA);
+            getArtifact(value);
         }
 
         mUpdateFormView = findViewById(R.id.create_artifact_form);
         mProgressView = findViewById(R.id.update_progress);
     }
 
-    private void chooseFile() {
+    private void getArtifact(final long id) {
+        showProgress(true);
+        SCFAsyncTask<ArtifactDTO> scfAsyncTask = new SCFAsyncTask<ArtifactDTO>() {
+            @Override
+            void onSuccess(ArtifactDTO value) {
+                //Toast.makeText(UpdateActivity.this, "Artifact is updated", Toast.LENGTH_SHORT).show();
+                showProgress(false);
+            }
+
+            @Override
+            protected void onFailure(Exception value) {
+                super.onFailure(value);
+                Toast.makeText(UpdateActivity.this, "Failure", Toast.LENGTH_SHORT).show();
+                showProgress(false);
+            }
+
+            @Override
+            ArtifactDTO inBackground() {
+                artifact = scfClient.getArtifact(id);
+                mNameView.setText(artifact.getName());
+                return artifact;
+            }
+        };
+        scfAsyncTask.execute();
+    }
+
+    private void chooseArtifact() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("*/*");
         intent.addCategory(Intent.CATEGORY_OPENABLE);
@@ -69,11 +112,55 @@ public class UpdateActivity extends AbstractActivity  {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                if (resultCode == RESULT_OK) {
+                    Uri uri = data.getData();
+                    try {
+                        String path = getPath(this, uri);
+                        mPathView.setText(path);
+                        Toast.makeText(this, path, Toast.LENGTH_SHORT).show();
+                    } catch (URISyntaxException e) {
+                        e.printStackTrace();
+                        Toast.makeText(this, "oops",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private String getPath(Context context, Uri uri) throws URISyntaxException {
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = { "_data" };
+            Cursor cursor = null;
+
+            try {
+                cursor = context.getContentResolver().query(uri, projection, null, null, null);
+                int column_index = cursor.getColumnIndexOrThrow("_data");
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+            } catch (Exception e) {
+                // Eat it
+            }
+        }
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
     private void updateArtifact() {
         boolean cancel = false;
         View focusView = null;
 
         final String name = mNameView.getText().toString();
+        final String path = mPathView.getText().toString();
 
         if (TextUtils.isEmpty(name)) {
             mNameView.setError(getString(R.string.error_field_required));
@@ -103,6 +190,12 @@ public class UpdateActivity extends AbstractActivity  {
                 ArtifactDTO inBackground() {
                     ArtifactDTO updatedArtifact = scfClient.getArtifact(artifact.getId());
                     updatedArtifact.setName(name);
+
+                    if (path != null && !path.equals("")) {
+                        File file = new File(path);
+                        return scfClient.updateArtifactFile(updatedArtifact, file);
+                    }
+
                     return scfClient.updateArtifact(updatedArtifact);
                 }
             };
